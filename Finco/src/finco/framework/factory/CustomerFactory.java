32afbb98773cc65e7f5f43b20e8483a7e427d319
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-03-20 12:50:56 -0500

    CustomerFactory

--- Finco/src/finco/framework/factory/CustomerFactory.java
+++ Finco/src/finco/framework/factory/CustomerFactory.java
@@ -1,10 +1,23 @@
 package finco.framework.factory;
 
+import finco.framework.party.ACustomer;
+import finco.framework.party.Company;
+import finco.framework.party.Person;
+
 /*
  * @Author Sumit Wankhede
  */
 
 
 public class CustomerFactory {
-
+	public static ACustomer getInstance(String type) {
+        switch (type) {
+            case "PERSON":
+                return new Person();
+            case "COMPANY":
+                return new Company();
+            default:
+                return null;
+        }
+    }
 }
