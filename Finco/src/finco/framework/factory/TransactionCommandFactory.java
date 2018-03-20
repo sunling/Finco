e2dd34f411091b1b1191f5ed708d94091e45f20e
Sumit Wankhede <sumit.wankhede@hotmail.com>
2018-03-20 13:00:59 -0500

    TransactionCommandFactory

---
+++ Finco/src/finco/framework/factory/TransactionCommandFactory.java
@@ -0,0 +1,25 @@
+package finco.framework.factory;
+
+import finco.framework.command.AddInterest;
+import finco.framework.command.DepositEntry;
+import finco.framework.command.Transaction;
+import finco.framework.command.WithdrawEntry;
+
+/*
+ * @Author Sumit Wankhede
+ */
+
+public class TransactionCommandFactory {
+	public static Transaction getInstance(String transactionType, String accountNo, double amount) {
+        switch (transactionType) {
+            case "DEPOSIT":
+                return new DepositEntry(accountNo, amount);
+            case "WITHDRAW":
+                return new WithdrawEntry(accountNo, amount);
+            case "ADD INTEREST":
+                return new AddInterest(amount);
+            default:
+                return null;
+        }
+    }
+}
